4f5cff89b1b5daa0adfdef724cf3f2ad69f25e10
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2022-01-19 10:32:08 -0300

    Sistema de câmera finalizado
    A câmera agora segue o player apenas depois que este ultrapassar limites pré-definidos no eixo x e/ou no eixo y, dando um certo espaço retangular para que o player se mova sem mover a câmera.

--- Scripts/CameraMotor.cs
+++ Scripts/CameraMotor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraMotor : MonoBehaviour
 {
-    public Transform lookAt; //Indica a direção do player a ser focado
+    public Transform lookAt; //Indica a direção do player a ser focado. Lembrar de adicionar o objeto Player manualmente no campo "look at" na interface do Unity
     public float boundX = 0.15f; //Indica até onde o player pode se mover no eixo x antes de a câmera começar a segui-lo
     public float boundY = 0.05f; //Indica até onde o player pode se mover no eixo y antes de a câmera começar a segui-lo
 
@@ -18,29 +18,43 @@ public class CameraMotor : MonoBehaviour
         
         //Diferença entre as distancias atuais no eixo x do lookAt, isto é, do player, e a deste objeto (CameraMotor)
         float deltaX = lookAt.position.x - transform.position.x;
-        //Condição em que o player se move além dos limites para a direita
-        if (deltaX > boundX){
-            deltaX = deltaX + boundX;
-        }
-        //Condição em que o player se move além dos limites para a esquerda
-        if (deltaX < -boundX){
-            deltaX = deltaX - boundX;
+
+        //Condição em que o player se move além dos limites para a direita ou para a esquerda
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            //Checa se o centro da câmera no eixo x é menor do que a posição do player no eixo x
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            //Checa se o centro da câmera no eixo x é maior do que a posição do player no eixo x. Observe que a condição de "igual" aqui não se aplica, pois já foi desconsiderada no if externo a este
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
         }
 
         //TESTANDO SE O PLAYER SE MOVE ALÉM DOS LIMITES DO EIXO Y:
 
         //Diferença entre as distancias atuais no eixo y do lookAt, isto é, do player, e a deste objeto (CameraMotor)
         float deltaY = lookAt.position.y - transform.position.y;
-        //Condição em que o player se move além dos limites para cima
-        if (deltaY > boundY){ 
-            deltaY = deltaY + boundY;
-        }
-        //Condição em que o player se move além dos limites para baixo
-        if (deltaY < -boundY){
-            deltaY = deltaY - boundY;
+
+        //Condição em que o player se move além dos limites para cima ou para baixo
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            //Checa se o centro da câmera no eixo y é menor do que a posição do player no eixo y
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            //Checa se o centro da câmera no eixo y é maior do que a posição do player no eixo y. Observe que a condição de "igual" aqui não se aplica, pois já foi desconsiderada no if externo a este
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
         }
 
-        transform.position += new Vector3(deltaX, deltaY, 0);
+        transform.position += new Vector3(delta.x, delta.y, 0);
 
     }
 }
